4acd3e986bee61596e4051fd39c31043c9abdf4e
Rathish Manivannan <rathish.manivannan@gmail.com>
2025-11-06 23:26:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     
-    return "Updated Version 7 Deployed Successfully via CI/CD through GCP!"
+    return "Updated Version 1 Deployed Successfully via CI/CD through GCP!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
